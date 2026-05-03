881f7000710ce0f07fe488e6310a64e8e2a41318
Robin636 <monika@rauner.de>
2026-05-03 17:31:12 +0200

    refinement Zeile hinzufügen

--- einvoiceProject/asgi.py
+++ einvoiceProject/asgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'einvoiceProject.settings')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'einvoiceProject.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'einvoiceProject.settings.dev'
 
 application = get_asgi_application()
